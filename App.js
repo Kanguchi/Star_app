4a643ed78261ef1c05013d122880d1f03bae9666
Kanguchi <70295418+Kanguchi@users.noreply.github.com>
2021-08-13 23:29:36 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
 
 const appStackNavigatior = createStackNavigator(
   {
-    home :{ HomeScreen, 
+    home :{ screen:HomeScreen, 
     navigationOptions:{
       headerShown: false,
     }
